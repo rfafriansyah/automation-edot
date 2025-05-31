05415edfb2df3d71d12f211cac070505a7889022
rafriansyah <rhamafahri17@gmail.com>
2025-05-31 15:48:37 +0700

    update scenarios dashboard

--- web/desktop/dashboard/scenarios/core.spec.ts
+++ web/desktop/dashboard/scenarios/core.spec.ts
@@ -1,59 +1,54 @@
+import { credentialsLogin } from "../../../../helper/dashboard/credentials";
 import { test } from "../pageObjects/Base.page";
 
-let username = `it.qa`
-let password = `it.QA2025`
-
-let subs = `337089`
-let extend = `esuite`
-let statusExtend = `Trial has been extended successfully`
+let subs = `337089`;
+let extend = `esuite`;
+let statusExtend = `Trial has been extended successfully`;
 
 test.describe.serial("Core  - Positive", () => {
   test.beforeEach(async ({ page, loginPage, navbarPage }) => {
-
     await loginPage.open();
-    await loginPage.clickbuttonLoginWithEdot()
-    await loginPage.filltxtEmailUsername(username)
-    await loginPage.clickbuttonLogin()
-    await loginPage.filltxtPassword(password)
-    await loginPage.clickbuttonLogin()
-    await navbarPage.clickmoduleCore()
-
+    await loginPage.clickbuttonLoginWithEdot();
+    await loginPage.filltxtEmailUsername(credentialsLogin().username);
+    await loginPage.clickbuttonLogin();
+    await loginPage.filltxtPassword(credentialsLogin().password);
+    await loginPage.clickbuttonLogin();
+    await navbarPage.clickmoduleCore();
   });
 
-  test("Validate Page", async ({ page, corePage, }) => {
-
-    await corePage.validatePage()
+  test("Validate Page", async ({ page, corePage }) => {
+    await corePage.validatePage();
   });
 
-  test("Validate Menu Subscription", async ({ page, corePage, }) => {
-
-    await corePage.validateMenuSubscription()
+  test("Validate Menu Subscription", async ({ page, corePage }) => {
+    await corePage.validateMenuSubscription();
   });
 
-  test("Added Subscriptions", async ({ page, corePage, }) => {
-
-    await corePage.clickMenuSubscription()
-    await corePage.clicksubmenuSubscriptions()
-    await corePage.filltxtSearchbarSubmenuSubscriptions(subs)
-    await page.waitForTimeout(1000)
-    await corePage.clickbuttonViewSubmenuSubscriptions()
-    await corePage.clickbuttonExtendTrialSubmenuSubscriptions()
-    await corePage.filltxtExtendSubmenuSubscriptions(extend)
-    await corePage.clickbuttonYesExtendSubmenuSubscriptions()
-    await corePage.validateStatus(statusExtend)
+  test("Added Subscriptions", async ({ page, corePage }) => {
+    await corePage.clickMenuSubscription();
+    await corePage.clicksubmenuSubscriptions();
+    await corePage.filltxtSearchbarSubmenuSubscriptions(subs);
+    await page.waitForTimeout(1000);
+    await corePage.clickbuttonViewSubmenuSubscriptions();
+    await corePage.clickbuttonExtendTrialSubmenuSubscriptions();
+    await corePage.filltxtExtendSubmenuSubscriptions(extend);
+    await corePage.clickbuttonYesExtendSubmenuSubscriptions();
+    await corePage.validateStatus(statusExtend);
   });
 
-  test("Added Quota User Grub", async ({ page, corePage, }) => {
+  test("Added Quota User Grub", async ({ page, corePage }) => {
+    let statusExtendQuota = `Quota has been extended successfully`;
 
-    let statusExtendQuota = `Quota has been extended successfully`
-
-    await corePage.clickMenuSubscription()
-    await corePage.clicksubmenuSubscriptions()
-    await corePage.filltxtSearchbarSubmenuSubscriptions(subs)
-    await page.waitForTimeout(1000)
-    await corePage.clickbuttonViewSubmenuSubscriptions()
-    await corePage.clickbuttonAllocationsSubmenuSubscriptions()
-    await corePage.clickbuttonExtendQuotaSubmenuSubscriptions(`1`, statusExtendQuota)
-    await corePage.validateStatus(statusExtendQuota)
+    await corePage.clickMenuSubscription();
+    await corePage.clicksubmenuSubscriptions();
+    await corePage.filltxtSearchbarSubmenuSubscriptions(subs);
+    await page.waitForTimeout(1000);
+    await corePage.clickbuttonViewSubmenuSubscriptions();
+    await corePage.clickbuttonAllocationsSubmenuSubscriptions();
+    await corePage.clickbuttonExtendQuotaSubmenuSubscriptions(
+      `1`,
+      statusExtendQuota
+    );
+    await corePage.validateStatus(statusExtendQuota);
   });
 });
--- web/desktop/dashboard/scenarios/login.spec.ts
+++ web/desktop/dashboard/scenarios/login.spec.ts
@@ -1,28 +1,24 @@
+import { credentialsLogin } from "../../../../helper/dashboard/credentials";
 import { test } from "../pageObjects/Base.page";
 
-let username = `it.qa`
-let password = `it.QA2025`
-
 test.describe.serial("Login Dashboard - Positive", () => {
-  test.beforeEach(async ({ page, loginPage, }) => {
-
+  test.beforeEach(async ({ page, loginPage }) => {
     await loginPage.open();
-    await loginPage.clickbuttonLoginWithEdot()
+    await loginPage.clickbuttonLoginWithEdot();
   });
 
-  test("User melakukan login menggunakan username", async ({ page, loginPage, }) => {
-
-    await loginPage.filltxtEmailUsername(username)
-    await loginPage.clickbuttonLogin()
-    await loginPage.filltxtPassword(password)
-    await loginPage.clickbuttonLogin()
-
+  test("User melakukan login menggunakan username", async ({
+    page,
+    loginPage,
+  }) => {
+    await loginPage.filltxtEmailUsername(credentialsLogin().username);
+    await loginPage.clickbuttonLogin();
+    await loginPage.filltxtPassword(credentialsLogin().password);
+    await loginPage.clickbuttonLogin();
   });
 
   test.afterEach(async ({ page, navbarPage, dashboardPage }) => {
-
-    await navbarPage.validatePage()
-    await dashboardPage.validatePage()
+    await navbarPage.validatePage();
+    await dashboardPage.validatePage();
   });
-
 });
