cc63f66f17a4461161502096fa7b359e52fdcba2
rafriansyah <rhamafahri17@gmail.com>
2025-05-31 13:32:02 +0700

    update

--- helper/generatePayload.ts
+++
@@ -1,9 +0,0 @@
-import { faker } from "@faker-js/faker";
-
-const randomName = faker.person.fullName(); // "Alice Johnson"
-const company = faker.company.name(); // "Global Corp"
-const phone = faker.phone.number(); // "(555) 123-4567"
-const email = faker.internet.email(); // "alice.johnson@example.com"
-const address = faker.location.streetAddress(); // "123 Maple Street"
-
-console.log({ randomName, company, phone, email, address });
