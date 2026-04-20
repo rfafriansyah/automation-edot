61ff7f6bc3dd9d1c0ebd3e7154d627f2eea6d268
rafriansyah <rhamafahri17@gmail.com>
2026-04-20 10:12:34 +0700

    updated

--- api/jest.setup.ts
+++
@@ -1,5 +0,0 @@
-import "jest-json-schema-extended";
-const { setup } = require("jest-json-schema-extended");
-setup();
-
-jest.setTimeout(100000);
--- api/notification/interfaces/segmentList.ts
+++
@@ -1,5 +0,0 @@
-export interface SegmentPayload {
-  segment_name: string;
-  description: string;
-  is_test: boolean;
-}
